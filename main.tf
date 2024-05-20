c65cad9c61062792b1637bd53f2550c7453671fd
Anu <143781093+anudhiman18@users.noreply.github.com>
2024-05-20 13:42:48 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-data "newrelic_test_grok_pattern" "grok"{
-    grok        = "%%{IP:host_ip}"
-    log_lines   = ["host_ip: 43.3.120.2"]
-}
 resource "newrelic_log_parsing_rule" "log-parsing"{
     name        = var.log-parsing.name
     attribute   = var.log-parsing.attribute
